3b6568d3aaab6399bb7b26007082167260c325bb
Jéssika Gomes <64968527+jessikagomes@users.noreply.github.com>
2021-06-27 19:03:14 -0300

    adding style

--- pizza.js
+++ pizza.js
@@ -1,20 +1,20 @@
-		function calcularPizza(){
-			var n = parseInt(document.getElementById("qtdPessoas").value);
-			var x = parseInt(document.getElementById("qtdPedacos").value);
-			var resultado = document.getElementById("tPizza");
-			var sabores = 0;
-			var result =0;
-			var extras=0;
+function calcularPizza(){
+	var n = parseInt(document.getElementById("qtdPessoas").value);
+	var x = parseInt(document.getElementById("qtdPedacos").value);
+	var resultado = document.getElementById("tPizza");
+	var sabores = 0;
+	var result =0;
+	var extras=0;
 
-			document.getElementById("qtdPessoas").value="";
-			document.getElementById("qtdPedacos").value="";
+	document.getElementById("qtdPessoas").value="";
+	document.getElementById("qtdPedacos").value="";
 
-			if (!!n && !!x) {
-				result = Math.ceil((n * x)/8);
-				sabores = result * 2;
-				extras = (result * 8) - (n*x);
-				return resultado.innerHTML = "Order " + result + " pizza(s) <br>Extra slices: " + extras + "<br>Order up to " + sabores + " toppings.";
-			} else {
-				return resultado.innerHTML = "* Please, fill in the fields."
-			}
+	if (!!n && !!x) {
+		result = Math.ceil((n * x)/8);
+		sabores = result * 2;
+		extras = (result * 8) - (n*x);
+		return resultado.innerHTML = "Peça <b>" + result + "</b> pizza(s) <br>Pedaços extra: <b>" + extras + "</b><br>Até <b>" + sabores + "</b> sabores.";
+		} else {
+			return resultado.innerHTML = "* Por favor, preencha os campos.<br><br><br>"
 		}
+}
